53302d11b20828ac6075abf49a9f8353d259178f
KevRWB <kevinrenaultoff@gmail.com>
2022-07-04 13:47:16 +0200

    Constraints SQL

--- Demo_SQL/CREATE_TABLE.sql
+++ Demo_SQL/CREATE_TABLE.sql
@@ -1,33 +1,36 @@
 -- Create table 
 
 CREATE TABLE Employes(
-	CodeEmp INT				not null CONSTRAINT PK_Employes PRIMARY KEY,			
+	CodeEmp INT				CONSTRAINT PK_Employes PRIMARY KEY --not null not useful -> PRIMARY KEY is required  
+							IDENTITY(1,1),	-- (1,1) is the default value for IDENTITY (not required to write)		
 	Nom VARCHAR(20)			not null,
 	Prenom CHAR(20)			not null,
 	DateNaissance DATE		null,
 	DateEmbauche DATE		not null CONSTRAINT DF_Employes_DateEmbauche DEFAULT getdate(),
 	DateModif TIMESTAMP		null,
-	Salaire NUMERIC(8,2)	not null CONSTRAINT DF_Employes_Salaire DEFAULT 0,	
+	Salaire NUMERIC(8,2)	not null	CONSTRAINT DF_Employes_Salaire DEFAULT 0
+										CONSTRAINT CK_Employes_Salaire CHECK(Salaire >= 0),	
 	CodeService CHAR(5)		not null,
-	CodeChef INT			null
+	CodeChef INT			null,
+	CONSTRAINT CK_Employes_VerifDate CHECK(DateNaissance is null OR DateNaissance >= DateEmbauche)
 );
 
 CREATE TABLE Services(
 	CodeService CHAR(5)		not null,
 	Libelle VARCHAR(30)		not null,
-	CONSTRAINT PK_Services PRIMARY KEY(CodeService)
+	CONSTRAINT PK_Services PRIMARY KEY(CodeService),
+	CONSTRAINT UN_Services_Libelle UNIQUE(Libelle)
 );
 
 CREATE TABLE Conges_Mens(
-	CodeEmp int					not null CONSTRAINT PK_Conges_Mens PRIMARY KEY,				
+	CodeEmp int					not null	CONSTRAINT PK_Conges_Mens PRIMARY KEY,				
 	Annee numeric(4,0)			not null,
-	Mois numeric(2,0)			not null,
-	NbJoursPris numeric(2,0)	null CONSTRAINT DF_Employes_NbJoursPris DEFAULT 0
+	Mois numeric(2,0)			not null	CONSTRAINT CK_Conges_Mens CHECK(Mois >= 1 AND Mois <=12),
+	NbJoursPris numeric(2,0)	null		CONSTRAINT DF_Conges_Mens_NbJoursPris DEFAULT 0
 );
 
 CREATE TABLE Conges(
-	CodeEmp int					not null,				
+	CodeEmp int					not null CONSTRAINT PK_Conges PRIMARY KEY,				
 	Annee numeric(4,0)			not null,
-	NbJoursAcquis numeric(2,0)	null CONSTRAINT Df_Employes_NbJoursAcquis DEFAULT 30,
-	CONSTRAINT PK_Conges PRIMARY KEY (CodeEmp)
+	NbJoursAcquis numeric(2,0)	null	CONSTRAINT Df_Employes_NbJoursAcquis DEFAULT 30
 );
