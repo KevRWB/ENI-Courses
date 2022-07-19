0ebcef56316c42e3fb5948aab6159a5a7d753136
KevRWB <kevinrenaultoff@gmail.com>
2022-07-19 09:43:17 +0200

    Correction tp maximot

--- Java_POO_ENI/javaPooIntro/src/fr/eni/ecole/baseDuLangage/JeuDeLettres.java
+++ Java_POO_ENI/javaPooIntro/src/fr/eni/ecole/baseDuLangage/JeuDeLettres.java
@@ -9,35 +9,50 @@ import java.util.Scanner;
 
 public class JeuDeLettres {
 
+	private static final String ADRESSE = "./dictionnaire.txt";
+	
 	public static void main(String[] args) {
+		
 		try {
-			char[] motAlea=tirerMotAleatoirement();
-			
-			afficher(motAlea);
-			char[] motMelanger=melanger(motAlea);
-			afficher(motMelanger);
-			System.out.println("Quelle est votre proposition ?");
-			Scanner sc =new Scanner(System.in);
-			char[] tabMotPropose=sc.nextLine().toUpperCase().toCharArray();
-			boolean ok=bonnesLettres(motMelanger,tabMotPropose) ;
-			System.out.println(ok);
-			
-
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			//un mot est tiré dans le dictionnaire
+			char[] mot = tirerMotAleatoirement();
+			//melange des lettres du mot -> Retour du nouveau mot mélangé (char[])
+			char[] motMelange = melanger(mot);
+			//afficher tirage au joueur
+			System.out.println("Voici le tirage :");
+			afficher(motMelange);
+			//permettre une proposition de mot
+			System.out.println("Quel mot proposez-vous ?");
+			Scanner input = new Scanner(System.in);
+			char[]prop = input.nextLine().toUpperCase().toCharArray();
+			//--check si les lettre de la proposition sont bonnes
+			if(!bonnesLettres(motMelange,prop)) {
+				System.out.println("Les lettres proposées sont incorrectes");
+			}else {
+				//--Check si mot dans le dico
+				if(dansLeDico(prop)) {
+					System.out.println("Le mot est bien dans le dictionnaire. Vous avez gagné !");
+					System.out.println(String.format("Vous avez obtenu %d points", prop.length));
+				}else {
+					System.out.println("Mot non présent dans le dictionnaire...");
+				}
+			}	
+		//catch de toutes le exceptions possibles
+		}catch(Exception e) {
+			System.out.println("Erreur lors de la lecture du dictionnaire");
+		}	
 	}
+	
+	//###  METHODS
+	//--######--METHODS
+	
+	//--Tirer mot aleatoirement dans le fichier dictionnaire
 	private static char[] tirerMotAleatoirement() throws FileNotFoundException, IOException {
 		char[] tableau=null;
 		try (
 				//charge le flux d'entrée
-				FileInputStream fichier = new FileInputStream("./dictionnaire.txt");
-				FileInputStream fichier2 = new FileInputStream("./dictionnaire.txt");
+				FileInputStream fichier = new FileInputStream(ADRESSE);
+				FileInputStream fichier2 = new FileInputStream(ADRESSE);
 				//je met le fux dans un scanner pour pouvoir le lire
 				Scanner s= new Scanner(fichier);
 				Scanner s2=new Scanner(fichier2);
@@ -69,57 +84,89 @@ public class JeuDeLettres {
 		// je retourne le tableau
 		return tableau;
 	}
-private static char[] melanger(char[] tabAmelanger) {
-	char[] tabMelange= new char[tabAmelanger.length];
-	// clonage du tableau
-	for(int i=0;i<tabAmelanger.length;i++)
-	{
-		tabMelange[i]=tabAmelanger[i];
-	}
-//	char[] tabMelange= Arrays.copyOf(tabAmelanger, tabAmelanger.length);
-	Random rnd = new Random();
-	for(int i=0;i<tabMelange.length;i++)
-	{
-		int positionLettre1=rnd.nextInt(tabMelange.length);
-		int positionLettre2=rnd.nextInt(tabMelange.length);
-		char tmp=tabMelange[positionLettre1];
-		tabMelange[positionLettre1]=tabMelange[positionLettre2];
-		tabMelange[positionLettre2]=tmp;
-	}
-	return tabMelange;
-}
-private static void afficher(char[] motAafficher)
-{
-//	for(int i=0;i<motAafficher.length;i++)
-//	{
-//		System.out.print(motAafficher[i]);
-//	}
-	System.out.println(motAafficher);
-
-}
-
-private static boolean bonnesLettres(char[]lettresDuTirage,char[] motJoueur) {
 	
-	char[] copie=Arrays.copyOf(lettresDuTirage, lettresDuTirage.length);
-	int j=0;
-	boolean ok=true;
-	while(ok && j<motJoueur.length)
-	{
-		int k=0;
-		while( k<copie.length && motJoueur[j]!=copie[k])
+	//---FONCTION MELANGER
+	private static char[] melanger(char[] tabAmelanger) {
+		char[] tabMelange= new char[tabAmelanger.length];
+		// clonage du tableau
+		for(int i=0;i<tabAmelanger.length;i++)
 		{
-			k++;
+			tabMelange[i]=tabAmelanger[i];
 		}
-		if (k==copie.length) {
-			ok=false;
+	//	char[] tabMelange= Arrays.copyOf(tabAmelanger, tabAmelanger.length);
+		Random rnd = new Random();
+		for(int i=0;i<tabMelange.length;i++)
+		{
+			int positionLettre1=rnd.nextInt(tabMelange.length);
+			int positionLettre2=rnd.nextInt(tabMelange.length);
+			char tmp=tabMelange[positionLettre1];
+			tabMelange[positionLettre1]=tabMelange[positionLettre2];
+			tabMelange[positionLettre2]=tmp;
 		}
-		else {
-			copie[k]=' ';
-			j++;
-		}
+		return tabMelange;
 	}
-	return ok;
-}
-
+	
+	//--FONCTION AFFICHER
+	private static void afficher(char[] motAafficher)
+	{
+		for(int i=0;i<motAafficher.length;i++)
+		{
+			System.out.print(motAafficher[i]);
+		}
+		System.out.println(motAafficher);
+	
+	}
+	
+	
+	//FONCTION CHECK SI BONNES LETTRES
+	private static boolean bonnesLettres(char[]lettresDuTirage,char[] motJoueur) {
+		
+		char[] copie=Arrays.copyOf(lettresDuTirage, lettresDuTirage.length);
+		int j=0;
+		boolean ok=true;
+		while(ok && j<motJoueur.length)
+		{
+			int k=0;
+			while( k<copie.length && motJoueur[j]!=copie[k])
+			{
+				k++;
+			}
+			if (k==copie.length) {
+				ok=false;
+			}
+			else {
+				copie[k]=' ';
+				j++;
+			}
+		}
+		return ok;
+	}
+	
+	//--CHECK SI MOTS SONT IDENTIQUES
+	private static boolean sontIdentiques(char[] mot1, char[] mot2) {
+		boolean ok = mot1.length == mot2.length;
+		if(ok) {
+			int i = 0;
+			while(ok && i<mot1.length) {
+				ok = mot1[i] == mot2[i];
+				i++;
+			}
+		}
+		return ok;
+	}
+	
+	//--CHECK SI MOT DANS LE DICO (UTILISANT FONCTION sontIdentiques)
+	private static boolean dansLeDico(char[]prop) throws FileNotFoundException, IOException {
+		boolean trouve=false;
+		try(FileInputStream fichier = new FileInputStream(ADRESSE);
+				Scanner s = new Scanner(fichier)){
+			char[] motDico;
+			while(s.hasNextLine() && !trouve) {
+				motDico = s.nextLine().toUpperCase().toCharArray();
+				trouve=sontIdentiques(prop, motDico);
+			}
+		}
+		return trouve;
+	}
 
 }
