37b57496baaf5bf74196a21850010b99885b611b
KevRWB <kevinrenaultoff@gmail.com>
2022-07-20 09:31:06 +0200

    Correction TP terminée

--- Java_POO_ENI/javaPooIntro/src/fr/eni/ecole/tpUseClass/CorrectinTpUseClass.java
+++ Java_POO_ENI/javaPooIntro/src/fr/eni/ecole/tpUseClass/CorrectinTpUseClass.java
@@ -7,35 +7,55 @@ import java.util.Scanner;
 public class CorrectinTpUseClass {
 
 	public static void main(String[] args) {
-		GregorianCalendar date = new GregorianCalendar();
-		afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
-		System.out.println("---------------");
-		System.out.println("***  Choisissez une option : ***");
-		System.out.println("     -------");
-		System.out.println("- mois précédent");
-		System.out.println("+ mois précédent");
-		System.out.println("+ mois et année de votre choix(mm/yyyy)");
-		System.out.println("q - Quitter");
-		System.out.println("--------------");
+		GregorianCalendar date = new GregorianCalendar();//Création d'un objet calendrier au mois et à l'année en cours
+		afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH)); // afficher calendrier
 		
-		Scanner input = new Scanner(System.in);
-		String inputUser = input.next();
-		
-		switch(inputUser) {
-		case "-": 
-			date.add(GregorianCalendar.MONTH, -1);
-			afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
-		break;
-		case "+": 
-			date.add(GregorianCalendar.MONTH, +1);
-			afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
-		break;
-		case "q":
-		break;
-		
-		default:
+		//declaration scanner
+		Scanner sc = new Scanner(System.in);
+		String inputUser;
+		//Boucle de programme
+		do {
+			//Affichage menu / consigne
+			System.out.println("---------------");
+			System.out.println("***  Choisissez une option : ***");
+			System.out.println("     -------");
+			System.out.println("- mois précédent");
+			System.out.println("+ mois précédent");
+			System.out.println("+ mois et année de votre choix(mm/yyyy)");
+			System.out.println("q - Quitter");
+			System.out.println("--------------");
+			//Scanner de saisie utilisateur
+			inputUser = sc.next();
 			
-		}
+			switch(inputUser) {
+			case "-": 
+				date.add(GregorianCalendar.MONTH, -1);
+				afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
+			break;
+			case "+": 
+				date.add(GregorianCalendar.MONTH, +1);
+				afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
+			break;
+			case "q":
+				System.out.println("Au revoir");
+			break;
+				
+			default:
+				if(inputUser.length()==7) {
+					//Substring de mm/yyyy
+					// On récupère mm avec substring - Converti en Int pour stockage dans variable
+					int mois = (Integer.valueOf(inputUser.substring(0,2)) -1); // -1 -> Mois commencent à zero dans GregorianCalendar
+					int annee = Integer.valueOf(inputUser.substring(3,7)); // valeur de année avec substring
+					//Changement des valeurs de l'objet GragorianCalendar "date" avec paramètres renseignés par utilisateur
+					date.set(annee, mois, 1);
+					afficherMois(date.get(GregorianCalendar.YEAR), date.get(GregorianCalendar.MONTH));
+				}else {
+					System.out.println("Saisie incorrecte");
+				}
+			}
+		}while(!inputUser.equalsIgnoreCase("q")); // tant que saisie différente de "q" (en ignorant majuscules)
+		
+		
 		
 	}
 	//### METHODS
@@ -44,6 +64,7 @@ public class CorrectinTpUseClass {
 		GregorianCalendar maDate = new GregorianCalendar(annee, mois, 1);
 		// affichage mois - année
 		System.out.println(String.format("* %s %d *",		
+				// getDisplayName = methode de l'instance "maDate"
 				maDate.getDisplayName(GregorianCalendar.MONTH, GregorianCalendar.LONG, Locale.FRANCE),
 				maDate.get(GregorianCalendar.YEAR)));
 		//affichage jour(String) semaine
@@ -65,16 +86,16 @@ public class CorrectinTpUseClass {
 		for(int i = 0; i<decalage; i++) {
 			System.out.print("   ");
 		}
-		//Dernier jour du mois
-		int dernierJourMoi = maDate.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+		//Nombre de jours maximum dans le mois en cours de l'instance "maDate"
+		int dernierJourMois = maDate.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
 		//affichage des jours
-		for (int i = 1; i <= dernierJourMoi ; i++) {
+		for (int i = 1; i <= dernierJourMois ; i++) {
 			System.out.printf("%02d ", maDate.get(GregorianCalendar.DAY_OF_MONTH));
 			//condition pour passer à la ligne - si dimanche
 			if(maDate.get(GregorianCalendar.DAY_OF_WEEK) == GregorianCalendar.SUNDAY) {
 				System.out.printf("%n");
 			}
-			//passe au joueur suivant
+			//passe au jour suivant -> Methode add de l'instance
 			maDate.add(GregorianCalendar.DAY_OF_MONTH, 1);
 		}	
 	}//END Fonction afficherMois
