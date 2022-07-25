8f996f6a42dcbdfbda91806cc92f4262e35bf118
KevRWB <99198628+KevRWB@users.noreply.github.com>
2022-07-25 23:08:36 +0200

    end tp cycles

--- Java_POO_ENI/tpLocationCycles/src/tpLocationCycles/TestCycles.java
+++ Java_POO_ENI/tpLocationCycles/src/tpLocationCycles/TestCycles.java
@@ -9,9 +9,9 @@ public class TestCycles {
 		Velo lapierre = new Velo("Lapierre", "speed 400", LocalDate.of(2021, 01, 5), 27 );
 		Velo btwin = new Velo("Btwin", "riverside 900", LocalDate.of(2022, 01, 5), 10 );
 		Gyropode segway = new Gyropode("Segway", "Ninebot",  LocalDate.of(2021, 01, 5), 40, "1m50");
-		Gyropode weebot = new Gyropode("Weebot", "Echo",  LocalDate.of(2020, 01, 5), 35, "1m60");
-		Gyroroue immotion = new Gyroroue("Immotion", "v8", LocalDate.of(2020, 01, 5), 40);
-		Gyroroue ninebot = new Gyroroue("Segway", "Ninebot One E+", LocalDate.of(2020, 01, 5), 30);
+		Gyropode weebot = new Gyropode("Weebot", "Echo",  LocalDate.of(2020, 11, 5), 35, "1m60");
+		Gyroroue immotion = new Gyroroue("Immotion", "v8", LocalDate.of(2021, 11, 5), 40);
+		Gyroroue ninebot = new Gyroroue("Segway", "Ninebot One E+", LocalDate.of(2022, 01, 5), 30);
 		
 		//AFFICHAGES
 		lapierre.afficher();
