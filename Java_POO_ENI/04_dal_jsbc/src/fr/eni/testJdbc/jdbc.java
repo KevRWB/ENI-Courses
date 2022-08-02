f7397ba25cc2d693df5e09d80218a4a205b66bef
KevRWB <99198628+KevRWB@users.noreply.github.com>
2022-08-02 08:17:58 +0200

    cours jdbc

---
+++ Java_POO_ENI/04_dal_jsbc/src/fr/eni/testJdbc/jdbc.java
@@ -0,0 +1,10 @@
+package fr.eni.testJdbc;
+
+import java.sql.DriverManager;
+
+public class jdbc {
+	public static void main(String[] args) {
+
+	}
+	
+}
---
+++ Java_POO_ENI/04_dal_jsbc/src/fr/eni/testJdbc/testJdbc.java
@@ -0,0 +1,12 @@
+package fr.eni.testJdbc;
+
+import java.sql.DriverManager;
+
+public class testJdbc {
+
+	public static void main(String[] args) {
+		
+		String urlConnection = "jdbc:sqlserver://192.168.0.41; databasename=DESKTOP-"
+	}
+
+}
