5298f92a51f10111e3ad84800d22ac29dd798718
KevRWB <99198628+KevRWB@users.noreply.github.com>
2022-10-04 21:47:57 +0200

    keep on train with da training website

---
+++ ENI-JS/VS/Training/TrainListenersContext/gamesScript.js
@@ -0,0 +1,10 @@
+/**
+ * Script Train on context and listeners
+ */
+
+let btnGuessNumber = document.getElementById("guessNumberBtn");
+let divGame = document.getElementById("game");
+
+btnGuessNumber.addEventListener('click', function(e){
+    e.target.style.backgroundColor = 'green';
+})
