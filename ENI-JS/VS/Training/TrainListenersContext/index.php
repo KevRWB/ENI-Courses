86b50fa822f44374126c521797f44f4557f1d7e0
KevRWB <99198628+KevRWB@users.noreply.github.com>
2022-10-05 07:01:43 +0200

    try php whith live server vs code -> nope -> need Mamp

--- ENI-JS/VS/Training/TrainListenersContext/index.php
+++ ENI-JS/VS/Training/TrainListenersContext/index.php
@@ -14,17 +14,7 @@
 
 <body>
     <header>
-        <div id="logo"></div>
-        <div id="txtHeader">
-            <h1>Da' training website</h1>
-        </div>
-        <nav>
-            <ul>
-                <a href="index.html"><li>Accueil</li></a>
-                <a href="games.html"><li>Jeux</li></a>
-                <li>Page 2</li>
-            </ul>
-        </nav>
+        <?php include('fragments/header.php'); ?>
     </header>
 
     <main>
