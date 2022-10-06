2e874762aa41213a18699bf7e3c5101d6d6aabc1
Kevin RENAULT <99198628+KevRWB@users.noreply.github.com>
2022-10-06 15:31:08 +0200

    Jquery lib

---
+++ ENI-JS/VS/ModuleJQuery/script.js
@@ -0,0 +1,14 @@
+/**
+ * Cours JQuery
+ * 
+ */
+
+//Fonction cacher toutes les div
+function hideAll(){
+    $('div').hide();
+}
+
+//Fonction afficher toutes les div
+function showAll(){
+    $('div').show();
+}
